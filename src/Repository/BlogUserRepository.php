d3f62b200ed7bca1bd0d0216efd49f86c0e5de18
Adam Černý <adamcerny0@gmail.com>
2020-08-10 23:08:47 +0200

    delete comments

--- src/Repository/BlogUserRepository.php
+++ src/Repository/BlogUserRepository.php
@@ -19,32 +19,4 @@ class BlogUserRepository extends ServiceEntityRepository
         parent::__construct($registry, BlogUser::class);
     }
 
-    // /**
-    //  * @return BlogUser[] Returns an array of BlogUser objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?BlogUser
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
