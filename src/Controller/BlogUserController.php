5c78626d4a7f0f9471115351f855e65cb2378d96
Adam Černý <adamcerny0@gmail.com>
2020-08-11 02:04:19 +0200

    clear unnecessary code

--- src/Controller/BlogUserController.php
+++ src/Controller/BlogUserController.php
@@ -13,6 +13,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+
 
 /**
  * @Route("/user")
@@ -139,10 +141,7 @@ class BlogUserController extends AbstractController
 
                 $user->setPassword($password);
                 $em->persist($user);
-                $flush = $em->flush();
-                if ($flush === null) {                
-                    return $this->redirectToRoute("main");
-                }
+                $em->flush();
             } 
         }
 
